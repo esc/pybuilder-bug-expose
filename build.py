41b87297dbfe6628bb29ba38ed8d10feca365f8c
Valentin Haenel <valentin@haenel.co>
2016-06-15 16:46:37 +0200

    adding installation of foo/bar

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
+use_plugin("copy_resources")
 
 
 default_task = "publish"
@@ -12,3 +13,6 @@ default_task = "publish"
 @init
 def initialize(project):
         project.build_depends_on('mockito')
+        project.set_property("copy_resources_target", "$dir_dist")
+        project.get_property("copy_resources_glob").append('foo/bar')
+        project.install_file("foo/bar", "foo/bar")
